4560c930e071f28e099e06f6cc49f72464f6d63c
lotcher <cb229435444@outlook.com>
2022-03-04 14:01:09 +0800

    feature: 添加Pycall调用jieba分词

--- src/Jieba.jl
+++ src/Jieba.jl
@@ -1,5 +1,19 @@
 module Jieba
+using PyCall
 
-# Write your package code here.
+export jieba
+export cut, cut_for_search
+
+const jieba = PyNULL()
+
+# https://github.com/JuliaPy/PyCall.jl#using-pycall-from-julia-modules
+function __init__()
+    copy!(jieba, pyimport("jieba"))
+end
+
+cut(msg; use_paddle = false, cut_all = false, HMM = false) = jieba.lcut(
+    msg, use_paddle = use_paddle, cut_all = cut_all, HMM = HMM
+)
+cut_for_search(msg; HMM = false) = jieba.lcut_for_search(msg, HMM = HMM)
 
 end
