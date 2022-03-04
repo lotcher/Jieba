ec9d0d65ead7c283182817a54cd8bf25456215e4
lotcher <cb229435444@outlook.com>
2022-03-04 14:52:51 +0800

    feature: 修改readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Jieba
+# Jieba.jl
+
+jieba分词的PyCall调用版本
+
+## Usage
+
+## 分词
+
+```julia
+using Jieba
+
+cut("我是真的大帅哥")   # ["我", "是", "真的", "大", "帅哥"]
+cut_for_search("卡梅隆安东尼戴维斯")  # ["卡梅隆", "安东", "安东尼", "维斯", "戴维斯"]
+```
+
+## 其他
+
+使用样例见test/runtests.jl
+
+```julia
+using Jieba
+
+jieba  # python: import jieba
+
+analyse  # python: import jieba.analyse
+
+pseg  # python: import jieba.posseg as pseg
+```
+
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,13 @@ using Test
 
 @testset "Jieba.jl" begin
     # 分词
-    cut("我是真的大帅哥") |> println
-    cut_for_search("卡梅隆安东尼戴维斯") |> println
+    @time cut("我是真的大帅哥") |> println
+    @time cut_for_search("卡梅隆安东尼戴维斯") |> println
 
     # 特征提取
-    analyse.extract_tags("提取TF-IDF特征向量") |> println
-    analyse.textrank("提取TextRank特征向量") |> println
+    @time analyse.extract_tags("提取TF-IDF特征向量") |> println
+    @time analyse.textrank("提取TextRank特征向量") |> println
 
     # 词性标注
-    pseg.lcut("我爱北京天安门") |> println
+    @time pseg.lcut("我爱北京天安门") |> println
 end
